97cad029102bcdd4b028bfea9ed33a14271b2252
Jan Simson <jan.simson@gmx.de>
2018-05-23 14:44:07 +0200

    add timestamps

--- supporting-files/database-setup.sql
+++ supporting-files/database-setup.sql
@@ -6,5 +6,7 @@ CREATE TABLE Registration (
     name varchar(255),
     confirmationCode varchar(32),
     confirmed tinyint(10),
-    unsubscribed tinyint(10)
+    unsubscribed tinyint(10),
+    registered_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+    last_changed_on DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
